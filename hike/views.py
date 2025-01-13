72af4ae563ed409356d9ffa047b2446deda8ee75
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-13 16:36:19 +0300

    Update views.py

--- hike/views.py
+++ hike/views.py
@@ -53,7 +53,7 @@ class BMAllListView(View):
         sform = SearchForm() 
         return render(request, 'hike/bm.html', {'form': form, 'sform': sform, 'objects': objects, })
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request, pks, *args, **kwargs):
         form = UdateForm(request.POST)
         if form.is_valid():
             order = form.save(commit=False)
