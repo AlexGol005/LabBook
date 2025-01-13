b020115af260613e2f6f6062f2226773d809ca4f
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-13 16:25:05 +0300

    Update views.py

--- hike/views.py
+++ hike/views.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from django.shortcuts import render
 import pandas as pd
+from django.shortcuts import redirect, get_object_or_404
 from django.views import View
 from django.views.generic import ListView, CreateView
 
